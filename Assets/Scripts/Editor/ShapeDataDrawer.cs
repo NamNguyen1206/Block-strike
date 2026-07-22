b68e887535706b9843b020da5f48b74c15f61aeb
NamNguyen <nguyenmai7922@gmail.com>
2026-07-22 13:31:12 +0700

    push

---
+++ Assets/Scripts/Editor/ShapeDataDrawer.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.UIElements;
+using Unity.VectorGraphics;
+
+[CustomEditor(typeof(ShapeData),false)]
+[CanEditMultipleObjects]
+[System.Serializable]
+public class ShapeDataDrawer : Editor
+{
+    private ShapeData ShapeDataInstance => target as ShapeData;
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        ClearBoardButton();
+        EditorGUILayout.Space();
+
+        DrawColumnsInputFields();
+        EditorGUILayout.Space();
+
+        if(ShapeDataInstance.board != null && ShapeDataInstance.columns > 0 && ShapeDataInstance.rows > 0)
+        {
+            DrawsBoardTable();
+        }
+
+        serializedObject.ApplyModifiedProperties();
+
+        if(GUI.changed)
+        {
+            EditorUtility.SetDirty(ShapeDataInstance);
+        }
+
+    }
+
+    private void ClearBoardButton()
+    {
+        if(GUILayout.Button("Clear Board"))
+        {
+            ShapeDataInstance.Clear();
+        }
+    }
+
+    private void DrawColumnsInputFields()
+    {
+        var columnTemp = ShapeDataInstance.columns;
+        var rowsTemp = ShapeDataInstance.rows;
+
+        ShapeDataInstance.columns = EditorGUILayout.IntField("Column", ShapeDataInstance.columns);
+        ShapeDataInstance.rows = EditorGUILayout.IntField("row", ShapeDataInstance.rows);
+
+        if((ShapeDataInstance.columns != columnTemp || ShapeDataInstance.rows != rowsTemp) &&
+            ShapeDataInstance.columns > 0 && ShapeDataInstance.rows > 0)
+        {
+            ShapeDataInstance.CreatNewBoard();
+        }
+    }
+
+    private void DrawsBoardTable()
+    {
+        var tableStyle = new GUIStyle("box");
+        tableStyle.padding = new RectOffset(10,10,10,10);
+        tableStyle.margin.left = 32;
+
+        var headerColumnStyle = new GUIStyle();
+        headerColumnStyle.fixedWidth = 65;
+        headerColumnStyle.alignment = TextAnchor.MiddleCenter;
+
+        var rowStyle = new GUIStyle();
+        rowStyle.fixedHeight = 25;
+        rowStyle.alignment = TextAnchor.MiddleCenter;
+
+        var dataFieldStyle = new GUIStyle(EditorStyles.miniButtonMid);
+        dataFieldStyle.normal.background = Texture2D.grayTexture;
+        dataFieldStyle.onNormal.background = Texture2D.whiteTexture;
+
+        for(var row = 0; row < ShapeDataInstance.rows; row++)
+        {
+            EditorGUILayout.BeginHorizontal(headerColumnStyle);
+            
+            for(var column = 0; column < ShapeDataInstance.columns; column++)
+            {
+                EditorGUILayout.BeginHorizontal(rowStyle);
+                var data = EditorGUILayout.Toggle(ShapeDataInstance.board[row].column[column],dataFieldStyle);
+                ShapeDataInstance.board[row].column[column] = data;
+                EditorGUILayout.EndHorizontal();
+
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        
+    }
+}
